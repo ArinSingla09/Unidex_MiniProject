b566bf3cdeedee5493f8b7659dfeb5d9665cf01b
ArinSingla09 <arin.singla.09@gmail.com>
2024-04-21 23:31:50 +0530

    correction

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import { FaUser, FaLock } from "react-icons/fa";
-import { useNavigate,Link } from "react-router-dom";
-
+import { FaHome } from "react-icons/fa";
 export default function Login(){
 
-    const [Email,setEmail]=useState('')
+    const [Username,setUsername]=useState('')
     const [Password,SetPassword] = useState('');
     async function submit(e){
         e.preventDefault();
         try{
             await axios.post("http://localhost:27017/Unidex.Unidex/",{
-                Email,Password
+                Username,Password
             })
         }
         catch(e){
@@ -19,16 +18,19 @@ export default function Login(){
     }
 
     return (
-        <div className='w-screen h-screen bg-[url("./assets/Background_home.png")] flex items-center justify-center bg-cover'>
-            <form action="POST" className="w-[29%] h-[70%] flex flex-col justify-around items-center rounded-2xl bg-[#D9D9D975] backdrop-blur-[1px]">
+        <div className='w-screen h-screen bg-[url("./assets/Background_home.png")] flex flex-col items-center justify-between bg-cover'>
+            <nav className=" w-full h-[10%]">
+                <a href="/"><FaHome size={55} className=" ml-4 mt-2"/></a>
+            </nav>  
+            <form action="POST" className="w-[29%] h-[70%] mb-[6%] flex flex-col justify-around items-center rounded-2xl bg-[#D9D9D975] backdrop-blur-[1px]">
                 <h1 className="font-temp2 text-3xl">Login</h1>
                 <div className="flex">
                     <input type="text" onchange={
-                        (e)=>{SetEmail(e.target.value)}} placeholder="Username" required className="w-[150%] h-7 p-3 py-5 mr-5" /><FaUser size={40}/>
+                        (e)=>{setUsername(e.target.value)}} placeholder="Username" required className="w-[150%] h-7 p-3 py-5 mr-5" /><FaUser size={40}/>
                 </div>
                 <div className="flex">
                     <input type="Password" onchange={
-                        (e)=>{SetPassword(e.target.value)}}placeholder="Password" required className="w-[150%] h-7 p-3 py-5 mr-5" /><FaLock size={40}/>
+                        (e)=>{setPassword(e.target.value)}}placeholder="Password" required className="w-[150%] h-7 p-3 py-5 mr-5" /><FaLock size={40}/>
                 </div>
                 <div className="w-[60%] flex justify-between">
                     <label className="text-sm"><input type="checkbox"  />Remember me</label> 
@@ -36,7 +38,7 @@ export default function Login(){
                 </div>
                 <button type="submit" onClick={submit} className=" w-24 h-10 bg-white">Login</button>
                 <div>
-                    <p>Don't have an account?<Link to="/signup">Sign up</Link></p>
+                    <p>Don't have an account?<a href="/signup">Sign up</a></p>
                 </div>
             </form>
         </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
 import { FaUser, FaLock } from "react-icons/fa";
-import { useNavigate,Link } from "react-router-dom";
+import { FaPhone } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
+import { FaHome } from "react-icons/fa";
+
 
 export default function Signup(){
 
+    const [Name,SetName] = useState('');
+    const [Number,SetNumber] = useState('');
     const [Email,setEmail]=useState('')
     const [Password,SetPassword] = useState('');
     async function submit(e){
@@ -19,24 +24,31 @@ export default function Signup(){
     }
 
     return (
-        <div className='w-screen h-screen bg-[url("./assets/Background_home.png")] flex items-center justify-center bg-cover'>
-            <form action="POST" className="w-[29%] h-[70%] flex flex-col justify-around items-center rounded-2xl bg-[#D9D9D975] backdrop-blur-[1px]">
+        <div className='w-screen h-screen bg-[url("./assets/Background_home.png")] flex flex-col items-center justify-between bg-cover'>
+            <nav className=" w-full h-[10%]">
+                <a href="/"><FaHome size={55} className=" ml-4 mt-2"/></a>
+            </nav>
+            <form action="POST" className="w-[29%] h-[70%] flex flex-col justify-around items-center rounded-2xl bg-[#D9D9D975] backdrop-blur-[1px] mb-[6%]">
                 <h1 className="font-temp2 text-3xl">SignUp</h1>
                 <div className="flex">
                     <input type="text" onchange={
-                        (e)=>{SetEmail(e.target.value)}} placeholder="Username" required className="w-[150%] h-7 p-3 py-5 mr-5" /><FaUser size={40}/>
+                        (e)=>{SetName(e.target.value)}} placeholder="Name" required className="w-[150%] h-7 p-3 py-5 mr-5" /><FaUser size={40}/>
+                </div>
+                <div className="flex">
+                    <input type="Number" onchange={
+                        (e)=>{SetNumber(e.target.value)}} placeholder="Phone Number" required min={0} className="w-[150%] h-7 p-3 py-5 mr-5" /><FaPhone size={40}/>
+                </div>
+                <div className="flex">
+                    <input type="email" onchange={
+                        (e)=>{SetEmail(e.target.value)}} placeholder="Email" required className="w-[150%] h-7 p-3 py-5 mr-5" /><IoMail size={40}/>
                 </div>
                 <div className="flex">
                     <input type="Password" onchange={
                         (e)=>{SetPassword(e.target.value)}}placeholder="Password" required className="w-[150%] h-7 p-3 py-5 mr-5" /><FaLock size={40}/>
                 </div>
-                <div className="w-[60%] flex justify-between">
-                    <label className="text-sm"><input type="checkbox"  />Remember me</label> 
-                    <a href="#" className="text-sm">Forgot Password?</a>
-                </div>
-                <button type="submit" onClick={submit} className=" w-24 h-10 bg-white">Login</button>
+                <button type="submit" onClick={submit} className=" w-24 h-10 bg-white">Sign Up</button>
                 <div>
-                    <p>Don't have an account?<Link to="/signup">Sign up</Link></p>
+                    <p>Have an account?<a href="/login">Login</a></p>
                 </div>
             </form>
         </div>
